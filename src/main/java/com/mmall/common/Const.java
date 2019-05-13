b2e15547fdfe14ac10c418cf04942331d9cdd3ef
LordJiang <daoyingdeyuanfang@163.com>
2019-05-13 18:37:04 +0800

    lf cart

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -1,5 +1,9 @@
 package com.mmall.common;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 public class Const {
     public static final String CURRENT_USER = "currentUser";
     public static final String EMAIL = "email";
@@ -9,4 +13,33 @@ public class Const {
         int ROLE_CUSTOMER = 0;  //普通用户
         int ROLE_ADMIN= 1;      //管理员
     }
+
+    public interface ProductListOrderBy{
+        Set<String> PRICE_ASC_DESC = Sets.newHashSet("order_desc","price_asc");
+    }
+
+    public interface Cart{
+        int CHECKED = 1;//购物车选中状态
+        int UNCHECKED = 0;//购物车未选中状态
+    }
+
+
+    public enum ProductStatusEnum{
+        ON_SALE(1,"在线");
+        private String value;
+        private int code;
+        ProductStatusEnum(int code,String value){
+            this.code = code;
+            this.value = value;
+        }
+
+        public String getValue(){
+            return value;
+        }
+
+        public int getCode(){
+            return code;
+        }
+    }
+
 }
